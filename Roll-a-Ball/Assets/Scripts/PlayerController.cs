d64e074a6c7988d3a520294c7dab012c4ade88e9
Yota Nakagawa <yota.nakagawa@suou.waseda.jp>
2017-10-25 06:51:41 +0900

    first commit

---
+++ Roll-a-Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    // speedを制御する
+    public float speed = 10;
+
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        float y =0;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            y = 1;
+        }
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+        // xとzにspeedを掛ける
+        rigidbody.AddForce(x * speed, y * speed, z * speed);
+    }
+}
